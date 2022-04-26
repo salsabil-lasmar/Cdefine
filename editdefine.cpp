5bb59eaa4184b7941d7ea04299de1140f9452689
salsabil-lasmar <95913542+salsabil-lasmar@users.noreply.github.com>
2022-04-26 11:54:25 +0100

    Update editdefine.cpp

--- editdefine.cpp
+++ editdefine.cpp
@@ -35,27 +35,22 @@ void EditDefine::on_pushButton_clicked()
         //QString path= ui->tableView->model()->data(ui->tableView->model()->index(row,3)).toString();;
         QString  oldname = ui->lineEdit_name->text();
         QString  path = ui->lineEdit_path->text();
+
         QFile file(path);
           if (file.open(QIODevice::ReadWrite | QIODevice::Text)) {
-              QTextStream stream (&file);
-              QRegularExpression startline("#define\\s*");
-              QRegularExpression endline("\\s*");
-              QRegularExpression regex (startline.pattern()+oldname+endline.pattern());
-              while(!stream.atEnd()){
-                  QString line = stream.readLine();
-                  QRegularExpressionMatch  regexMatcher = regex.match(line);
-                  if(regexMatcher.hasMatch()){
-                      QString newline = line.replace(oldname , NewDefineName);
-                      qDebug () << newline ;
-
-                    }
+              QString  s= file.readAll();
+              s.replace(oldname, NewDefineName);
+              QFile newfile(path);
+                  if(newfile.open(QFile::WriteOnly | QFile::Truncate)) {
+                      QTextStream out(&newfile);
+                      out <<s;
+                  }
+                  newfile.close();
+       }
                    /* else
                        QMessageBox::information(this,"Error","error editing define" );*/
-             }
-      }
       file.close();
       CDefineList obj;
       obj.setDefineList(path);
 }
-
 
