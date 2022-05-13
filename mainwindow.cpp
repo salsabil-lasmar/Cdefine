e5dd37b59a16fa7b52c5701809a5b6227e266a42
salsabil-lasmar <95913542+salsabil-lasmar@users.noreply.github.com>
2022-05-13 11:19:05 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,34 +2,46 @@
 #include "ui_mainwindow.h"
 #include "cdefinelist.h"
 #include"cdefine.h"
-#include"addnewdefine.h"
+#include"addwindow.h"
 #include "deletedefine.h"
-#include "editdefine.h"
+#include "editwindow.h"
 
 #include<QDebug>
+#include<QFile>
+#include<QVBoxLayout>
+#include<QObject>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     model = new QStandardItemModel();
+    loadData();
+
+     ui->tableView->setContextMenuPolicy(Qt::CustomContextMenu);
+
+     connect(ui->tableView, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(slotCustomMenuRequested(QPoint)));
+
+}
+
+void MainWindow::loadData()
+{
+    model->clear();
     model->setHorizontalHeaderItem(0, new QStandardItem("Name"));
     model->setHorizontalHeaderItem(1, new QStandardItem("Value"));
-    model->setHorizontalHeaderItem(2, new QStandardItem("File_type"));
+    model->setHorizontalHeaderItem(2, new QStandardItem("Path"));
 
     ui->tableView->setModel(model);
-
     CDefineList obj;
     obj.init();
     QList<QStandardItem *> list_items;
     for(auto itr = obj.mDefineList.begin(); itr != obj.mDefineList.end() ; ++itr) {
-             list_items.append(new QStandardItem(itr->name));
-             list_items.append(new QStandardItem(itr->value));
-             list_items.append(new QStandardItem(itr->type_file));
+         list_items.append(new QStandardItem(itr->name));
+         list_items.append(new QStandardItem(itr->value));
+         list_items.append(new QStandardItem(itr->type_file));
 
-             model->appendRow(list_items);
-             list_items.clear();
+         model->appendRow(list_items);
+         list_items.clear();
      }
 }
 
@@ -38,28 +50,69 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::table_view(){
-    ui->tableView->model();
-}
-
 void MainWindow::on_Add_pushButton_clicked()
 {
-    AddNewDefine* addNewDefine=new  AddNewDefine();
+    AddNewDefine* addNewDefine=new  AddNewDefine(this);
     addNewDefine->show();
 
 }
 
-void MainWindow::on_Delete_pushButton_clicked()
+
+void MainWindow :: slotCustomMenuRequested(QPoint pos)
 {
-    DeleteDefine* deletedefine=new  DeleteDefine(nullptr , ui);
-    deletedefine->show();
+
+
+    QMenu *contextMenu = new QMenu(this);
+    QAction * deleteAct = new QAction(tr("delete"), this);
+    QAction * editAct = new QAction(tr("edit"), this);
+
+    connect(deleteAct, SIGNAL(triggered()), this,SLOT(SlotDeleteDefine()));
+    connect(editAct, SIGNAL(triggered()), this, SLOT(SlotEditDefine()));
+
+    contextMenu->addAction(deleteAct);
+    contextMenu->addAction(editAct);
+
+    contextMenu->popup(ui->tableView->viewport()->mapToGlobal(pos));
+
+
 }
 
 
-void MainWindow::on_Edit_pushButton_clicked()
+void MainWindow::SlotDeleteDefine()
 {
-    EditDefine* editDefine=new EditDefine(nullptr , ui);
+     const QModelIndex index = ui->tableView->selectionModel()->currentIndex();
+    int row =index.row();
+   QString name  = ui->tableView->model()->data(ui->tableView->model()->index(row,0)).toString();
+   QString path = ui->tableView->model()->data(ui->tableView->model()->index(row,2)).toString();
+
+   QFile file(path);
+
+   if (file.open(QIODevice::ReadWrite | QIODevice::Text)) {
+
+     QTextStream stream (&file);
+     QRegularExpression startline  (".*#define\\s*");
+     QRegularExpression  endline ("\\s*.*");
+     QRegularExpression regex (startline.pattern() + name + endline.pattern());
+     QString s;
+     while(!stream.atEnd()){
+
+         QString line =stream.readLine();
+         QRegularExpressionMatch regexMatcher = regex.match(line);
+         if(!regexMatcher.hasMatch())
+             s.append(line + "\n");
+      }
+      file.resize(0);
+      stream << s;
+      file.close();
+ }
+
+   loadData();
+}
+
+void MainWindow::SlotEditDefine()
+{
+    class EditDefine* editDefine=new class EditDefine(nullptr , ui);
     editDefine->show();
-
 }
+
 
